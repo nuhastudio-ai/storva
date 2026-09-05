46c123c2066ce9f7220d92c8f6cf04e2c078ea96
nuhastudio-ai <nuhastudio.01@gmail.com>
2026-09-05 17:52:49 +0700

    Lay-1a0

--- apps/web/src/components/dashboard-shell.tsx
+++ apps/web/src/components/dashboard-shell.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Link from 'next/link'
 import { Sidebar, HeroCard, StorageCards, RecentFilesTable, FoldersCard, RightPanel } from './dashboard'
 import { DevicePicker } from './device-picker'
